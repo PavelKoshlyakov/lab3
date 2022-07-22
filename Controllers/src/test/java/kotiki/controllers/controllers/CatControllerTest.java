210281ec476a4dd3e1d695f429163d3bcdb801e9
Pavel_Koshlyakov <pkoshlyakov1@gmail.com>
2022-07-22 13:47:57 +0300

    docker

--- Controllers/src/test/java/kotiki/controllers/controllers/CatControllerTest.java
+++ Controllers/src/test/java/kotiki/controllers/controllers/CatControllerTest.java
@@ -3,6 +3,7 @@ package kotiki.controllers.controllers;
 import kotiki.controllers.controllers.CatController;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
